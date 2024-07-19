b61c213943b4fcf5c0387b734bbb37725cab2d61
inshafbloodshed <143176101+inshafbloodshed@users.noreply.github.com>
2024-07-19 20:35:47 +0530

    initial

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class root(App):
+    def build(self):
+        la=BoxLayout(orientation='vertical')
+        lb=Label(text="hello there")
+        la.add_widget(lb)
+        return la
+
+if __name__=="__main__":
+    root().run()
+
